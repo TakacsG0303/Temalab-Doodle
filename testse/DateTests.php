a173160c8bbb5aa76afb9ce343e5f2b8e9294ed1
Gizella Takács <takacs.gizella0303@gmial.com>
2022-03-07 21:59:41 +0100

    datumok között min 1h van test kész

--- testse/DateTests.php
+++ testse/DateTests.php
@@ -4,6 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class DateTests extends TestCase
 {
-    
+    public function test_dates_between_min_1h_01(){
+        $this->assertTrue([\DateValidates::class,'datesBetweenMin1h'('2022-03-07T10:56:50.725Z','2022-03-07T20:15:50.725Z')]);
+    }
+    public function test_dates_between_min_1h_02(){
+        $this->assertFalse([\DateValidates::class,'datesBetweenMin1h'('2022-03-07T10:56:50.725Z','2022-03-07T11:00:50.725Z')]);
+    }
 }
 ?>
