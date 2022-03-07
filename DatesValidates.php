9355f10d1b015032cabc4e44e05262b3d2ad7e72
Gizella Takács <takacs.gizella0303@gmial.com>
2022-03-07 21:47:38 +0100

    DateValidates class függvényei létrehozva

---
+++ DatesValidates.php
@@ -0,0 +1,15 @@
+<?php
+
+class DateValidates(){
+    public function datesBetweenMin1h($startDate, $endDate){
+
+    }
+    public function endDateAfterStartDate($startDate, $endDate){
+        
+    }
+    public function endDateLaterNowDate($endDate){
+        
+    }
+}
+
+?>
